63ac2dd6ce53fa3034554f4decd2805c298dadbd
nautonk <nautonk@gmail.com>
2022-10-10 10:02:35 +0700

    initial commit

--- GoogleImageScraper.py
+++ GoogleImageScraper.py
@@ -75,7 +75,7 @@ class GoogleImageScraper():
         self.number_of_images = number_of_images
         self.webdriver_path = webdriver_path
         self.image_path = image_path
-        self.url = "https://www.google.com/search?q=%s%s&source=lnms&tbm=isch&tbs=itp:clipart,isz:l&sa=X&ved=2ahUKEwie44_AnqLpAhUhBWMBHUFGD90Q_AUoAXoECBUQAw&biw=1920&bih=947" % (
+        self.url = "https://www.google.com/search?q=%s%s&source=lnms&tbm=isch&tbs=ic:trans,ift:png,itp:clipart,isz:l&sa=X&ved=2ahUKEwie44_AnqLpAhUhBWMBHUFGD90Q_AUoAXoECBUQAw&biw=1920&bih=947" % (
         search_key, extra_search_params)
         self.headless = headless
         self.min_resolution = min_resolution
